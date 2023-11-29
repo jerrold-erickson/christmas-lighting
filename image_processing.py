214fb536549d2d9a65e358d6a05c9d486932113f
Jerrold Erickson <jerickson510@gmail.com>
2023-11-28 21:56:12 -0800

    Implement Camera_Manager class for capturing images

--- image_processing.py
+++ image_processing.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from datetime import datetime
+import time
 
 CAM_SETTINGS = {
     cv2.CAP_PROP_FOURCC: cv2.VideoWriter_fourcc(*"MPJG"),
@@ -20,35 +21,49 @@ M_SIZE = 30
 M_THICKNESS = 5
 
 
-def brightest_region(image: np.ndarray, blur_size: int = 21):
+class Camera_Manager:
+    def __init__(self, cam_settings: dict = CAM_SETTINGS):
+        self.vc = cv2.VideoCapture(0)
+        self.settings = cam_settings
+
+        for setting in self.settings:
+            self.vc.set(setting, self.settings[setting])
+
+        t = time.time()
+        while not self.vc.isOpened():
+            time.sleep(0.1)
+            if time.time() - t > 5.0:
+                raise RuntimeError("Failed to open camera.")
+
+    def __del__(self):
+        self.vc.release()
+
+    def capture(self):
+        rval, frame = self.vc.read()
+        if not rval:
+            print("Failed to capture image.")
+            return None
+
+        return frame
+
+
+def brightest_region(image: np.ndarray, save: bool = False, blur_size: int = 21):
     img_greyscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     img_greyscale = cv2.GaussianBlur(img_greyscale, (blur_size, blur_size), 0)
     *_, max_indx = cv2.minMaxLoc(img_greyscale)
-    cv2.drawMarker(image, max_indx, M_COLOR, cv2.MARKER_SQUARE, M_SIZE, M_THICKNESS)
 
-    filename = f"{datetime.now().strftime('%m-%d_%H-%M-%S')}.png"
-    cv2.imwrite(filename, image)
+    if save:
+        cv2.drawMarker(image, max_indx, M_COLOR, cv2.MARKER_SQUARE, M_SIZE, M_THICKNESS)
+        filename = f"{datetime.now().strftime('%m-%d_%H-%M-%S')}.png"
+        cv2.imwrite(filename, image)
 
-
-def capture_image():
-    capture = cv2.VideoCapture(0)
-
-    for setting in CAM_SETTINGS:
-        capture.set(setting, CAM_SETTINGS[setting])
-
-    if not capture.isOpened():
-        print("Failed to open camera.")
-        return
-
-    rval, frame = capture.read()
-    if not rval:
-        print("Failed to capture image.")
-        return
-
-    capture.release()
-    return frame
+    return max_indx
 
 
 if __name__ == "__main__":
-    img = capture_image()
-    brightest_region(img)
+    camera = Camera_Manager()
+
+    for _ in range(5):
+        img = camera.capture()
+        coords = brightest_region(img)
+        print(f"Brightest region: {coords}")
