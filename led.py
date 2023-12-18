73eb0920b99a1578310023c6b2bdef5c167e4fc9
Jerrold Erickson <jerickson510@gmail.com>
2023-12-17 18:45:11 -0800

    Update LED_Manager brigthness handling

--- led.py
+++ led.py
@@ -7,18 +7,22 @@ import numpy as np
 
 class LED_Manager:
     DEFAULT_LEDS = 50
-    DEFAULT_BRIGHTNESS = int(0.3 * 255)
 
-    def __init__(
-        self, n: int = DEFAULT_LEDS, brightness: int = DEFAULT_BRIGHTNESS
-    ) -> None:
-        self._leds = neopixel.NeoPixel(board.D18, n)
-        self._num_pixels = n
-        self.brightness = brightness
+    def __init__(self, n: int = DEFAULT_LEDS, brightness: float = 0.1) -> None:
+        self._leds = neopixel.NeoPixel(board.D18, n, brightness=brightness)
+        self._n = n
 
     @property
-    def num_pixels(self) -> int:
-        return self._num_pixels
+    def num_LEDs(self) -> int:
+        return self._n
+
+    @property
+    def brightness(self) -> float:
+        return self._leds.brightness
+
+    @brightness.setter
+    def brightness(self, value: float) -> None:
+        self._leds.brightness = value
 
     def fill(self, color: tuple) -> None:
         assert len(color) == 3, "Color must be a tuple of length 3."
@@ -29,56 +33,37 @@ class LED_Manager:
 
     def __setitem__(self, index: int, color: tuple) -> None:
         assert len(color) == 3, "Color must be a tuple of length 3."
-        s = max(sum(color), 1)
-        color = [int((c / s) * self._brightness) for c in color]
 
         self._leds[index] = color
 
     def off(self) -> None:
         self.fill((0, 0, 0))
 
-    @property
-    def brightness(self) -> int:
-        return self._brightness
-
-    @brightness.setter
-    def brightness(self, value: int) -> None:
-        assert value in range(10, 256), "Brightness must be in range [10, 255]."
-
-        self._brightness = value
-        for i in range(self._num_pixels):
-            self[i] = self[i]
-
     def random(self) -> None:
-        colors = (
-            np.random.dirichlet(np.ones(3), size=self._num_pixels) * self._brightness
-        )
-        for i in range(self._num_pixels):
+        colors = np.random.dirichlet(np.ones(3), size=self._n) * 255
+        for i in range(self._n):
             self[i] = colors[i]
 
 
 if __name__ == "__main__":
     num_leds = 200
-    brightness = 125
-    pixels = LED_Manager(n=num_leds, brightness=brightness)
-
-    # pixels.off()
+    brightness = 0.3
+    LED = LED_Manager(n=num_leds, brightness=brightness)
 
     length = int(num_leds * 0.8)
 
     for head in range(length):
-        color = np.random.dirichlet(np.ones(3)) * 255
-        pixels[head] = color
-        time.sleep(0.05)
+        color = (np.random.dirichlet(np.ones(3)) * 255).astype(int)
+        LED[head] = color
 
     head += 1
     tail = head - length
 
     while True:
-        color = np.random.dirichlet(np.ones(3)) * 255
-        pixels[head] = color
-        pixels[tail] = (0, 0, 0)
-        time.sleep(0.05)
+        color = (np.random.dirichlet(np.ones(3)) * 255).astype(int)
+        LED[head] = color
+        LED[tail] = (0, 0, 0)
+        time.sleep(0.02)
 
         head += 1
         tail += 1
--- light_mapping.py
+++ light_mapping.py
@@ -9,17 +9,17 @@ NUM_LIGHTS = 200
 
 if __name__ == "__main__":
     camera = Camera_Manager()
-    pixels = LED_Manager(n=NUM_LIGHTS, brightness=BRIGHTNESS)
+    LED = LED_Manager(n=NUM_LIGHTS, brightness=BRIGHTNESS)
 
-    pixels.brightness = BRIGHTNESS
+    LED.brightness = BRIGHTNESS
 
     data = {}
 
-    for i in range(pixels.num_pixels):
-        pixels[i] = COLOR_ON
+    for i in range(LED.num_LEDs):
+        LED[i] = COLOR_ON
         time.sleep(0.5)
         img = camera.capture()
-        pixels[i] = (0, 0, 0)
+        LED[i] = (0, 0, 0)
         coords = brightest_region(img, save=True, save_name=f"data/pixel_{i}_y.png")
         print(f"Pixel {i}: {coords}")
 
@@ -28,5 +28,5 @@ if __name__ == "__main__":
     fname = "data/y_coords.csv"
     with open(fname, "w") as f:
         f.write("index,x,y\n")
-        for i in range(pixels.num_pixels):
+        for i in range(LED.num_LEDs):
             f.write(f"{i},{data[i][0]},{data[i][1]}\n")
